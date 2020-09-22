9289a02343c8c86281b8e7e0c3a7aff485bc4fe2
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-22 14:43:12 +0700

    Add project files.

---
+++ animal/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace animal
+{
+    class Animal
+    {
+        public string name;
+        public  string shound;
+        public string idnum;
+        public static int numOfAnimals = 0;
+
+        public Animal()
+        {
+        }
+
+        public Animal(string name,string shound, string idnum)
+        {
+            this.name = name;
+            this.shound = shound;
+            this.idnum = idnum;
+           
+        }
+        public int numofAnimals()
+        {
+            return numOfAnimals++;
+        }
+        public string MakeSound() {
+
+            return name + shound;
+        }
+
+    }
+}
---
+++ animal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int d = 0;
+            Animal a = new Animal();
+            while (d >= 0)
+            {
+                d++;
+                Console.WriteLine("Nhap ten dong vat: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Nhap tieng keu dong vat: ");
+                string shound = Console.ReadLine();
+                Console.WriteLine("id: "+a.numofAnimals()+"  "+name + " se keu " + shound);
+                
+            }
+
+        }
+        }
+    }
